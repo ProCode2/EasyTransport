6e7f56f1891a1f9c3f5d468367c488462db25ae1
MazumdarAyush07 <mazumdarayush.2002@gmail.com>
2020-09-08 19:14:29 +0530

    added navbar

---
+++ FrontEnd/src/Components/Nav.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from "react";
+import "./Nav.css";
+const Nav = () => {
+  const [nav, setNav] = useState(false);
+  useEffect(() => {}, [nav]);
+  return (
+    <div className="navbar">
+      <h1 className="logo">Easy Transport</h1>
+      <ul className="list">
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+        <li>Feedback</li>
+      </ul>
+      <div className="ham">
+        <div className="ham-menu">
+          <h1 className="" onClick={() => setNav(!nav)}>
+            {nav ? "X" : "="}
+          </h1>
+        </div>
+        <ul className={"mob-list" + (nav ? " display" : " display-none")}>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Feedback</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Nav;
