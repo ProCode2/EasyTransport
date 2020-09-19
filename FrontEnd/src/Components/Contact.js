90a7034439ef6bd768b9adc57136c9f3c9242e4d
ProCode2 <bpro249@gmail.com>
2020-09-19 04:37:45 -0700

    fixed email alignment

--- FrontEnd/src/Components/Contact.js
+++ FrontEnd/src/Components/Contact.js
@@ -7,23 +7,17 @@ export default function Contact() {
   return (
     <div className="contact">
       <h1 className="contact-header">Contact</h1>
-      <h4 className="contact-details">
+      <h4 className="contact-detail">
         Contact the Developer.
-        <br />
       </h4>
-      <p1 className="contact-Number">
+      <div className="contact-details">
         <Phone />
-        Phone No. : 8420833011
-        <br />
-      </p1>
-      <p2 className="contact-Email">
-        <br />
-        <br />
+        <p>Phone No: 8420833011</p>
+      </div>
+      <div className="contact-details">
         <Email />
-        Email id:
-      </p2>
-      <br />
-      <p3> mazumdarayush.2002@gmail.com</p3>
+        <p>Email id: mazumdarayush.2002@gmail.com</p>
+      </div>
     </div>
   );
 }
