b5ffc8935146940d5c8abb3cc5eef22de48e4c83
MazumdarAyush07 <mazumdarayush.2002@gmail.com>
2020-09-22 17:04:01 +0530

    Started new Server code

--- FrontEnd/src/Components/About.js
+++ FrontEnd/src/Components/About.js
@@ -8,7 +8,7 @@ export default function About() {
       <h5 className="about-para">
         This is a project for the Design Thinking and Innovation paper.
       </h5>
-      <p1>
+      <p>
         This is the back bone to the new mobile device based application to
         locate a particular public transport on the map and estimate its time of
         arrival to the detected destination.
@@ -17,7 +17,7 @@ export default function About() {
         The app will work on a Global Positioning System(GPS), tracking every
         vehicle in the proximity and try to judge the time required to reach a
         point with its current average speed.
-      </p1>
+      </p>
     </div>
   );
 }
--- server/server.js
+++ server/server.js
@@ -1,29 +1,2 @@
 const express = require("express");
 const cors = require("cors");
-
-Eta app = express() er pore:
-app.use(cors());
-const app = express();
-
-const database = [
-  {
-    vehicle: "bus",
-    location: [54.0, 12.1],
-  },
-  {
-    vehile: "bus",
-    location: [57.0, 15.1],
-  },
-  {
-    vehile: "bus",
-    location: [55.0, 9.2],
-  },
-  {
-    vehile: "bus",
-    location: [58.0, 13.7],
-  },
-];
-app.get("/locs", (req, res) => {
-  res.json(database);
-});
-app.listen(3001, () => console.log("listening on port 3001"));
